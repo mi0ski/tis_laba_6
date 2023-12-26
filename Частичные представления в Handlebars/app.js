e8079b42abe7e109f851e7b454f11b6e0822137d
Kira <Mi0ski@yandex.com>
2023-12-26 08:55:42 +0300

    sorry

---
+++ Частичные представления в Handlebars/app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const hbs = require('hbs');
+const app = express();
+
+app.set('view engine', 'hbs');
+hbs.registerPartials(__dirname + '/views/partials');
+console.log(__dirname + '/views/partials')
+app.use('/contact', function (request, response) {
+    response.render(__dirname + '/views/contact.hbs', {
+        title: 'Мои контакты',
+        emailsVisible: true,
+        emails: ['gavgav@mycorp.com', 'mioaw@mycorp.com'],
+        phone: '+1234567890',
+    });
+});
+
+app.use('/', function (request, response) {
+    response.render(__dirname + '/views/home.hbs');
+});
+
+app.listen(3000);
